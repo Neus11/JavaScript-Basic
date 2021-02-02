113c569f96fd4321c4827cf2f254b2686fadad80
Neus Calvo <neuscalvo11@gmail.com>
2021-02-02 13:20:46 +0100

    Adding Exercise 1 qnd 2 from Function and Literals of level 2

---
+++ JS Nivell 2/Name Print/Exercici 1/namePrint.js
@@ -0,0 +1,13 @@
+// Imprimir el nom i cognoms de l'usuari en variables mitjançant template literals,
+// creant les variables i referenciant en la impressió de l'missatge
+
+
+const n = 'Jhon';
+const s = 'Doe';
+const l = 'Masmellow';
+
+console.log(`The username name is ${n} ${s} ${l}`);
+
+
+
+
---
+++ JS Nivell 2/Name Print/Exercici 2/printLiteral.js
@@ -0,0 +1,7 @@
+// Invocar la funció mitjançant template literals
+
+const abc = function printName(a, b, c) {
+    console.log(`Username is ${a} ${b} ${c}`);
+}
+
+abc(`Jane`, `Doe`, `Mashmellow`);
